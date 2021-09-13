35f2b766c2f762df76870ba3920fe05b57001db5
miladiashe <dkdud0122@naver.com>
2021-09-14 00:37:19 +0900

    Revert "토스트 토스"
    This reverts commit 4ffcb8fef36abc9a0705ba7aaa340ed26a3b5416.

--- RecorderRunnable/app/src/main/java/com/grace/recorderrunnable/MLcode.kt
+++ RecorderRunnable/app/src/main/java/com/grace/recorderrunnable/MLcode.kt
@@ -93,6 +93,7 @@ class MLcode(private val context: Context) {
         // Post-processing: find the digit that has the highest probability
         // and return it a human-readable string.
         val result = output[0]
+        Toast.makeText(this.context, "ㅗㅗㅗㅗㅗㅗㅗ", Toast.LENGTH_SHORT).show()
         return result > 0.5
     }
 
